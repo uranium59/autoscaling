b6e8e84445e16977c781580c11eb4a4d19207485
uranium59 <gdzergling@core-a.org>
2017-12-07 02:23:27 +0900

    worker modify

--- worker.js
+++ worker.js
@@ -1,13 +1,16 @@
 var rand = Math.random;
+var staticnum
 var workMain = function(){
-	var i1 = rand() * 100000;
-	var i2 = rand() * 100000;
+	var i1 = parseInt(rand() * 50000);
+	var i2 = parseInt(rand() * 50000);
 	var arr = new Array(i2);
 	for(var i = 0; i < i1; ++i){
-		for(var j = 0; j < i2; ++j{
+		for(var j = 0; j < i2; ++j){
 			arr[j] = rand()* 10000 + '' + rand() *10000;
+			staticnum = i / j;
+			if(rand() > 0.94) break;
 		}
 	}
 }
 
-setInterval(workMain, 1);
+setInterval(workMain, 2);
